b273a184d70f5f516822bbb9caed4466baeaf092
Ali Cebe <alicebe86@gmail.com>
2025-03-14 10:07:14 -0400

    edited

--- deploy.js
+++ deploy.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import { publish } from 'gh-pages';
-
-
-const repositoryUrl = 'https://github.com/AliCebe1986/alicebe.git';
+const repositoryUrl = 'https://github.com/AliCebe1986/Portfolio.git';
 
 publish('dist', {
   branch: 'gh-pages',
@@ -17,22 +14,3 @@ publish('dist', {
   }
   console.log('Website deployed successfully to GitHub Pages!');
 });
-=======
-
-import { publish } from 'gh-pages';
-const repositoryUrl = 'https://github.com/AliCebe1986/alicebe.git';
-
-publish('dist', {
-  branch: 'gh-pages',
-  repo: repositoryUrl,
-  message: 'Auto-generated deployment to GitHub Pages',
-  dotfiles: true,
-  silent: false
-}, (err) => {
-  if (err) {
-    console.error('Error deploying to GitHub Pages:', err);
-    return;
-  }
-  console.log('Website deployed successfully to GitHub Pages!');
-});
->>>>>>> 579559c151a31223f95349f3da5f609c0f493711
